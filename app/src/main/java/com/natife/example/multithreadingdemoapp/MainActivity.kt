cc7811987fd849ca8173e75781c7c0960c6a4156
Tornado512 <zhadkoroman@gmail.com>
2022-01-26 15:23:41 +0200

    init commit

---
+++ app/src/main/java/com/natife/example/multithreadingdemoapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.natife.example.multithreadingdemoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
